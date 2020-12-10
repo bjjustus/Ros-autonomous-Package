85ea204271cc672cb4e28723fcb8037714788fa3
chansidney <75762388+chansidney@users.noreply.github.com>
2020-12-09 16:33:23 -0800

    Update ROS_hsv.py

--- camera/ROS_hsv.py
+++ camera/ROS_hsv.py
@@ -31,7 +31,7 @@ cv2.createTrackbar('lowV', 'sliders', lowV, 255, callback)
 cv2.createTrackbar('highV', 'sliders', highV, 255, callback)
 
 #publisher
-cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=10)
+cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 move=Twist()
 while not rospy.is_shutdown():
     ret, frame = cap.read()
