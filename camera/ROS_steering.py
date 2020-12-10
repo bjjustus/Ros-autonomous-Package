01fbf41334b5e95413bb9cdef506cfd071f33352
bjjustus <75348793+bjjustus@users.noreply.github.com>
2020-12-10 12:47:13 -0800

    Update ROS_steering.py

--- camera/ROS_steering.py
+++ camera/ROS_steering.py
@@ -14,15 +14,14 @@ def callback(msg):
          x=msg.linear.x
          z=msg.angular.z
          steering = convert_trans_rot_vel_to_steering_angle(z)
-	 return steering
+	 print(steering)
+	 #add steering
 def driver():
     #subscriber gets cmd_vel values from open cv file
     rospy.init_node('car')
-    steering=rospy.Subscriber('/cmd_vel', Twist, callback)
-    print(steering)
-    #pwm(steering)
+    rospy.Subscriber('/cmd_vel', Twist, callback)
     rospy.spin()
+    
 	
 #insert throttle
-while not rospy.is_shutdown():
-    driver()
+driver()
