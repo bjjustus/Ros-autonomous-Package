e55730fc779fbce032a6c5a36e2d3722aa244dfd
bjjustus <75348793+bjjustus@users.noreply.github.com>
2020-12-10 12:57:51 -0800

    Update ROS_steering.py

--- camera/ROS_steering.py
+++ camera/ROS_steering.py
@@ -5,21 +5,24 @@ from geometry_msgs.msg import Twist
 
 msg=Twist()
 #initialize kit
+kit = ServoKit(channels=16) 
 
-def convert_trans_rot_vel_to_steering_angle(omega):
-    #turns angular.z value into an angle
-    return 12*omega+90
+#def convert_trans_rot_vel_to_steering_angle(omega):
+#    #turns angular.z value into an angle
+#    return 12*omega+90
 
 def callback(msg):
-         x=msg.linear.x
-         z=msg.angular.z
-         steering = convert_trans_rot_vel_to_steering_angle(z)
-	 print(steering)
-	 #add steering
+#    x=msg.linear.x
+    z=msg.angular.z
+    steering = 12*z+90
+    print(steering)
+    #add steering
+    kit.servo[1].angle = steering
 def driver():
     #subscriber gets cmd_vel values from open cv file
     rospy.init_node('car')
     rospy.Subscriber('/cmd_vel', Twist, callback)
+    kit.continuous_servo[2].throttle = 0.05
     rospy.spin()
     
 	
